7f6075e60e36a5d2d50c0c5ffaf4f615c16c4dfd
Chong Liu <chong.liu@westfieldinvestment.com>
2020-06-22 13:13:31 -0400

    saving

---
+++ emacs.bat
@@ -0,0 +1,15 @@
+@ SET WORKON_HOME=C:\Users\cliu.WFI\AppData\Local\Continuum\Anaconda3\envs
+@ SET PYTHONPATH=C:\Users\cliu.WFI\Desktop\WFI\pylib\libs
+@ SET PATH=%PATH%;C:\Users\cliu.WFI\Desktop\WFI\pylib\libs\webdriver;C:\Users\cliu.WFI\AppData\Local\Continuum\anaconda3\Library\bin\graphviz
+@ SET AUTH_LOGOUT=https://wfiubuntu01/auth/oauth/logout
+@ SET AUTH_LOGIN=https://wfiubuntu01/auth/oauth/login
+@ SET AUTH_LOGIN_REFRESH=https://wfiubuntu01/auth/oauth/login_refresh
+@ SET AUTH_USER=https://wfiubuntu01/auth/oauth/user
+@ SET WKHTMLTOPDF_PATH=C:\Users\cliu.WFI\Desktop\WFI\pylib\libs\wkhtmltopdf\bin\wkhtmltopdf.exe'
+@ SET ZIPLINE_ROOT=C:\Users\cliu.WFI\Desktop\WFI\pylib\systematic trading\
+@ SET PYTHONSTARTUP=C:\Users\cliu.WFI\Desktop\WFI\pylib\startup.py
+@ SET WFIAPPLOGFOLDER=C:\Users\cliu.WFI\Desktop\WFI\pylib\logs\wfiapps\
+@ SET GPGBINARY=C:\Users\cliu.WFI\Desktop\WFI\pylib\libs\GnuPG\pub\gpg.exe
+@ SET GPGHOMEDIR=C:\Users\cliu.WFI\Desktop\WFI\pylib\libs\GnuPGHome
+@ SET SECRET_KEY=WESTFIELDBULL
+@ "C:\Program Files\Emacs\emacs-26.2_no_dep\bin\runemacs.exe" --debug-init
